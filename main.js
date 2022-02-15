e7214fd2ad13d243894b426abcf5406b2045d16b
khaled5005 <khaledamro6@hotmail.com>
2022-02-15 17:18:48 +0200

    challenges

---
+++ main.js
@@ -0,0 +1,28 @@
+const arr1 = ['Khaled','Ahmad','ch','shadi']
+
+const sayHello = function (givenArr){
+for (let i = 0; i < givenArr.length; i++) {
+    console.log("Hello " +givenArr[i]);
+    
+}
+}
+//  sayHello(arr1)
+
+ let num1 = 5
+ let num2 = num1
+ num2 -=3
+//  console.log(num1);
+//  console.log(num2);
+ 
+
+ const formatArr = function (arr) {
+     for (let i = 0; i < arr.length; i++) {
+         arr[i] = "Hello " +  arr[i];
+     }
+ }
+
+//  formatArr(arr1)
+//  console.log(arr1);
+
+
+
